1b22437d82550888d4a8e1cc10c80fb604cb1b13
Holger Schmidt <Holger.Schmidt@multicom.de>
2019-12-29 09:45:02 +0100

    Initial Commit

---
+++ PeakFinder/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PeakFinder
+//
+//  Created by Holger Schmidt on 29.12.19.
+//  Copyright © 2019 Holger Schmidt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
