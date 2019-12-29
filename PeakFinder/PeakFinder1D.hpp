fd2c4cc02c03c0736f3c666aa88f434c9829faf2
Holger Schmidt <Holger.Schmidt@multicom.de>
2019-12-29 16:41:19 +0100

    peak finder for 1d

---
+++ PeakFinder/PeakFinder1D.hpp
@@ -0,0 +1,46 @@
+//
+//  PeakFinder1D.hpp
+//  PeakFinder
+//
+//  Created by Holger Schmidt on 29.12.19.
+//  Copyright © 2019 Holger Schmidt. All rights reserved.
+//
+// find a peak value in a given range
+// a peak is a peak if and only if an element is greater or equal than its neigbours
+// if the element is the first or last element in the range than its enough to be greater or equal than the sole neighbor
+// every non empty range has at least one peak
+// use devide and conquer
+// complexity is O(log n)
+
+#ifndef PeakFinder1D_hpp
+#define PeakFinder1D_hpp
+
+#include <algorithm>
+
+template <typename IterT>
+IterT FindAPeak(IterT it, IterT end) {
+    auto distance = std::distance(it, end);
+    if (distance == 0)
+        return end;
+    if (distance == 1)
+        return it;
+    if (distance == 2) {
+        auto next = it;
+        ++next;
+        if (*it >= *next)
+            return it;
+        return next;
+    }
+    auto middleIndex = distance / 2;
+    auto middle = it;
+    std::advance(middle, middleIndex);
+    auto next = middle;
+    ++next;
+    if (*middle >= *next)
+        return FindAPeak(it, next);
+    if (*next >= *middle)
+        return FindAPeak(middle, end);
+    return middle;
+}
+
+#endif /* PeakFinder1D_hpp */
--- PeakFinder/main.cpp
+++ PeakFinder/main.cpp
@@ -7,9 +7,45 @@
 //
 
 #include <iostream>
+#include <vector>
+
+#include "PeakFinder1D.hpp"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::vector<int> ints0;
+    auto p0 = FindAPeak(ints0.cbegin(), ints0.cend());
+    if (p0 != ints0.cend())
+        std::cout << "0 found " << *p0 << "\n";
+    else
+        std::cout << "0 no peak found\n";
+    
+    std::vector<int> ints1 {1};
+    auto p1 = FindAPeak(ints1.cbegin(), ints1.cend());
+    if (p1 != ints1.cend())
+        std::cout << "1 found " << *p1 << "\n";
+    else
+        std::cout << "1 no peak found\n";
+    
+    std::vector<int> ints2 {1, 2};
+    auto p2 = FindAPeak(ints2.cbegin(), ints2.cend());
+    if (p2 != ints2.cend())
+        std::cout << "2 found " << *p2 << "\n";
+    else
+        std::cout << "2 no peak found\n";
+    
+    std::vector<int> ints3 {3, 2, 1};
+    auto p3 = FindAPeak(ints3.cbegin(), ints3.cend());
+    if (p3 != ints3.cend())
+        std::cout << "3 found " << *p3 << "\n";
+    else
+        std::cout << "3 no peak found\n";
+    
+    std::vector<int> ints4 {100, 80, 60, 50, 20};
+    auto p4 = FindAPeak(ints4.cbegin(), ints4.cend());
+    if (p4 != ints4.cend())
+        std::cout << "4 found " << *p4 << "\n";
+    else
+        std::cout << "4 no peak found\n";
+    
     return 0;
 }
